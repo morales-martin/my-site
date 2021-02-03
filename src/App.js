ac259ddc5eb94cce9ddef5c913e3905475bee236
Martin Morales <morales-martin@outlook.com>
2021-02-03 14:59:26 -0600

    Updated navbar, added HeroSection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-// import './App.css';
-import Navbar from './components/Navbar'
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
